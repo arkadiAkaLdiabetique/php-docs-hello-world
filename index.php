f4702e05075878075f54f44f4dff3eb1132095bb
arkadi <129153098+arkadiAkaLdiabetique@users.noreply.github.com>
2023-04-26 20:53:21 +0200

    Update index.php

--- index.php
+++ index.php
@@ -51,10 +51,19 @@
 </head>
 <body>
 	<h1>Login</h1>
-	<form method="post" action="">
+	<form method="post" action="login.php">
 		<label for="username">Username:</label>
 		<input type="text" id="username" name="username" required><br>
 		<label for="password">Password:</label>
 		<input type="password" id="password" name="password" required><br>
 		<input type="submit" value="Login">
 	</form>
+
+	<?php 
+		if(isset($_POST['username']) && isset($_POST['password'])){
+			header("Location: login.php");
+			
+			
+
+</body>
+</html>
