d390c5b8cf66efa04c49e1c8511a116249c9265b
arkadi <129153098+arkadiAkaLdiabetique@users.noreply.github.com>
2023-04-26 20:52:45 +0200

    Create login.php

---
+++ login.php
@@ -0,0 +1,58 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Troll Spinner</title>
+	<style type="text/css">
+		body {
+			background-color: #333;
+			color: #fff;
+			font-family: monospace;
+			font-size: 24px;
+		}
+		#header {
+			text-align: center;
+			margin-top: 50px;
+			font-size: 48px;
+			text-shadow: 2px 2px #000;
+			letter-spacing: 10px;
+			animation: spin 5s linear infinite;
+		}
+		@keyframes spin {
+			from { transform: rotate(0deg); }
+			to { transform: rotate(360deg); }
+		}
+		#troll {
+			margin: 0 auto;
+			animation: spin 5s linear infinite;
+		}
+	</style>
+</head>
+<body>
+	<div id="header">Root|FInder$ Best Team Ever</div>
+	<div id="troll">
+		<pre>
+                        _____
+                            .-'     '-.
+                           /     .     \
+                          /      |o|      \
+                         /   O       O   \
+                        /                \
+                       / ',          ,'   \
+                      /   ', `.  .' ,`     \
+                     /      ',`'`',`'       \
+                    /_,-~`--.~-.~--.~-._,-~`\
+                   /                       \
+                  /                         \
+                 /                           \
+                |    @@@@@   *****   @@@@@   |
+                |   @@@@@@@@*******@@@@@@@  |
+                \@@@@@@@@@*******@@@@@@@@@/
+                 \@@@@@@@@*******@@@@@@@@/
+                  \@@@@@@@@*****@@@@@@@@/
+                   `@@@@@@@@*@@@@@@@@@'
+                     `@@@@@@@@@@@@@@@'
+                        `@@@@@@@@@'
+		</pre>
+	</div>
+</body>
+</html>
